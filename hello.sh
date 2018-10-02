c11d8fe0feb8d590b6354b9335f11eb0c9448af7
Jun Sakata <j.sakata@dr-ubie.com>
2018-10-02 12:20:14 +0900

    fix volume name

--- hello.sh
+++ hello.sh
@@ -2,5 +2,5 @@
 docker volume create k8s-hands-on
 docker container run --rm -it \
      -v /var/run/docker.sock:/var/run/docker.sock \
-     -v fargate-handson:/root/config \
+     -v k8s-hands-on:/root/config \
      -p 8080:8080 sakajunquality/k8s-hands-on
